642d572f82a05478da35e1c0ecde0d97051fadde
Siddharth Tiwari <150476061+siddharthtiwar0403@users.noreply.github.com>
2026-08-25 01:08:52 +0530

    Time: 14 ms (28.59%), Space: 46.6 MB (19.12%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.toLowerCase().replaceAll("[^a-z0-9]","");
+        s = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
         int left = 0;
         int right = s.length() - 1;
         while(left<right){
@@ -11,11 +11,5 @@ class Solution {
             right --;
         }
         return true;
-    }
-    public static void main(String[] args) {
-        Solution S = new Solution();
-        System.out.println(S.isPalindrome("A man, a plan, a canal: Panama"));
-        System.out.println(S.isPalindrome("race a car"));
-        System.out.println(S.isPalindrome(" "));
     }
 }
